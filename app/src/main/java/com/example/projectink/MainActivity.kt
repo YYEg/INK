94bb4e7487121c0a122233ac85ab7807456f02c8
Egor <yyrkovskii@mail.ru>
2023-11-19 22:53:51 +0800

    bottomNavBar

--- app/src/main/java/com/example/projectink/MainActivity.kt
+++ app/src/main/java/com/example/projectink/MainActivity.kt
@@ -1,22 +1,46 @@
 package com.example.projectink
 
+import Fragments.ConnectionFragment
+import Fragments.HomeFragment
+import Fragments.MessageFragment
 import Fragments.SettingsFragment
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.Fragment
+import com.example.projectink.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val settingsFragment = SettingsFragment()
-        loadFragment(settingsFragment)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.navHome -> replaceFragment(HomeFragment())
+                R.id.navNotification -> replaceFragment(MessageFragment())
+                R.id.navProfile -> replaceFragment(ConnectionFragment())
+                R.id.navSearch -> replaceFragment(SettingsFragment())
+
+                else -> {
+
+                }
+
+            }
+            true
+        }
+
         //startActivity(Intent(this, BaseActivity::class.java))
     }
 
-    private fun loadFragment(settingsFragment: SettingsFragment) {
-        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.home_container, settingsFragment)
-        transaction.commit()
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.home_container, fragment)
+        fragmentTransaction.commit()
     }
 }
